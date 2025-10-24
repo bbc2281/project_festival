48b9fed25799875f8d9eceec7719a83ce1e6cfd9
bae <bgttttt2121@gmail.com>
2025-10-24 15:34:07 +0900

    리뷰 기능 html 연동 전

--- src/main/resources/static/js/review.js
+++ src/main/resources/static/js/review.js
@@ -1,7 +1,26 @@
 const btn = document.getElementById(`btnAddReview`);
-const btnModify = document.getElementById(`btnModifyReview`)
 const textarea = document.getElementById(`reviewText`);
 const uploadFile = document.getElementById(`upload_file`);
+
+document.querySelectorAll(".review-idx").forEach(el => {
+  const reviewIdx = el.dataset.reviewIdx;
+  console.log("리뷰 번호:", reviewIdx);
+});
+
+
+document.querySelectorAll(".btnModifyReview").forEach(btn => {
+  btn.addEventListener("click", () => {
+    const reviewIdx = btn.dataset.reviewIdx;
+    const content = btn.dataset.reviewContent;
+
+    textarea.value = content;
+
+    btnModify.dataset.reviewIdx = reviewIdx;
+    btn.style.display = "none";
+    btnModify.style.display = "inline-block";
+  });
+});
+
 
 btn.addEventListener(`click`, ()=>{
     console.log("리뷰작성실행됨.");    
@@ -43,7 +62,50 @@ btn.addEventListener(`click`, ()=>{
     console.error("에러", err);
     alert("요청 중 오류가 발생했습니다.");
     });
-})
+});
+
+btnModify.addEventListener(`click`, ()=>{
+  console.log("수정요청.")
+  const content = textarea.value;
+  if (!content) {
+    alert(`리뷰를 입력하세요`);
+    textarea.focus();
+    return;
+  }
+
+  const formData = new FormData();
+  const reviewIdx = btnModify.dataset.reviewIdx; 
+  formData.append("review_idx", reviewIdx);      
+  formData.append("review_content", content);
+  const file = uploadFile.files[0];
+  if (file) formData.append("upload_file", file);
+
+  fetch("/review/modify", {
+    method: "POST",
+    body: formData
+  })
+    .then(async response => {
+      if (!response.ok) {
+        const text = await response.text();
+        console.error("서버 응답 오류:", text);
+        alert("서버 오류 발생: " + response.status);
+        return;
+      }
+
+      const data = await response.json();
+      if (data.success) {
+        alert("리뷰가 수정되었습니다.");
+        location.reload();
+      } else {
+        alert(data.message);
+      }
+    })
+    .catch(err => {
+      console.error("에러", err);
+      alert("요청 중 오류가 발생했습니다.");
+    });
+
+});
 
 
 
