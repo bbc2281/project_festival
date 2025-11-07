b1db886d1f4dcfae8c221f58914c14962267667f
annabel <fffffdddddaai@gmail.com>
2025-11-07 20:42:21 +0900

    fix: restore official Kakao address API setup
    This change restores the correct structure for the Kakao Postcode API.
    The previous modification was not made by me.

---
+++ src/main/resources/static/js/myaddress.js
@@ -0,0 +1,33 @@
+function execDaumPostcode() {
+  new daum.Postcode({
+    oncomplete: function(data) {
+      let addr = ''; 
+      let extraAddr = '';
+
+      if (data.userSelectedType === 'R') {
+        addr = data.roadAddress;
+      } else {
+        addr = data.jibunAddress;
+      }
+
+      if (data.userSelectedType === 'R') {
+        if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
+          extraAddr += data.bname;
+        }
+        if (data.buildingName !== '' && data.apartment === 'Y') {
+          extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+        }
+        if (extraAddr !== '') {
+          extraAddr = ' (' + extraAddr + ')';
+        }
+        document.getElementById("extraAddress").value = extraAddr;
+      } else {
+        document.getElementById("extraAddress").value = '';
+      }
+
+      document.getElementById('postcode').value = data.zonecode;
+      document.getElementById("address").value = addr;
+      document.getElementById("detailAddress").focus();
+    }
+  }).open();
+}
