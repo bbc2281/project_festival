772e928ad0d74bb3cb0e283c694015c5b5905971
wanbae <dhksqo@naver.com>
2025-11-21 09:20:59 +0900

    수정

--- src/main/resources/static/py/news.py
+++ src/main/resources/static/py/news.py
@@ -13,7 +13,7 @@ response = requests.get(url, headers=headers)
 
 soup = BeautifulSoup(response.content, 'html.parser')
 # 발표당일 클래스명 확인(확인위치 최하단 a링크)
-elements = soup.select('.nwfSPmNIlDyRhvW6TUmz')
+elements = soup.select('.MhHMvACwlVBbjWsd7yi3')
 
 print(elements)
 
